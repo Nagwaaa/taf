fdb0dfe3145257719646addefa7045fe1d657a26
Nagwaaa <nagwaibrahim994@gmail.com>
2023-11-10 16:36:25 +0200

    modify

---
+++ src/main/java/pages/test.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.junit.Test;
+
+import java.io.Console;
+
+public class test {
+
+
+    @Test
+    public void testOne()
+    {
+        System.out.println("Nagwaaaa");
+    }
+}
